a02a3c8ae7315b8667c2543ff8600adf74eeb886
Lykan55 <c4073@oic.jp>
2025-02-19 21:24:05 +0900

    タイトルの更新
    スペースのUIの更新
    BGMの再生
    SEの再生

--- Assets/okuno/Script/Title.cs
+++ Assets/okuno/Script/Title.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Title : MonoBehaviour
 {
     [SerializeField] private GameObject helpUi;
-
-    [Header("効果音")]
-    [SerializeField] private AudioSource audioSource;
-    private int spaceCount = 0;
+    [SerializeField] private GameObject spaceUi;
     // フェードの処理があるなら、それが終わったときにtrueにしてもらう
     [SerializeField] private bool isSpace = false;
+
+    [Header("オーディオ系")]
+    [SerializeField] private AudioSource bgmAudioSource;
+    [SerializeField] private AudioSource seAudioSource;
+    private int spaceCount = 0;
 
     void Update()
     {
@@ -18,13 +20,14 @@ public class Title : MonoBehaviour
             {             
                 if (spaceCount == 0)
                 {
-                    audioSource.PlayOneShot(audioSource.clip);
+                    seAudioSource.PlayOneShot(seAudioSource.clip);
                     spaceCount++;
+                    spaceUi.SetActive(false);
                     helpUi.SetActive(true);
                 }
                 else if(spaceCount == 1) 
                 {
-                    audioSource.PlayOneShot(audioSource.clip);
+                    seAudioSource.PlayOneShot(seAudioSource.clip);
                     spaceCount++;
                 }
                 // この下にシーンのロードの命令
--- Assets/okuno/Script/UiBlink.cs
+++ Assets/okuno/Script/UiBlink.cs
@@ -18,7 +18,7 @@ public class UiBlink : MonoBehaviour
 
     void Update()
     {
-        sin = Mathf.Sin(2 * Mathf.PI * f * Time.time + Mathf.PI / 2);
+        sin = Mathf.Sin(2 * Mathf.PI * f * Time.time + Mathf.PI / 2);// + Mathf.PI / 2はアルファ値の開始を強制的に255からにするため
         // byteに変換し、範囲を0～255にする
         byte alpha = (byte)((1 + sin) / 2 * 255);
         newColor = image.color;

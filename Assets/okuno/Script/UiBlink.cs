c589ed3a891a567d7c15959d3fdddc0c88df7b98
Lykan55 <c4073@oic.jp>
2025-02-19 17:30:00 +0900

    奥野の更新
    クレーンのプレハブの更新
    タイトルシーンの更新（アニメーション・点滅）

---
+++ Assets/okuno/Script/UiBlink.cs
@@ -0,0 +1,29 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UiBlink : MonoBehaviour
+{
+    [SerializeField] private float T;
+    //[SerializeField] private float furehaba;
+    private Color newColor;
+    private Image image;
+    private float f;
+    private float sin;
+
+    void Start()
+    {
+        f = 1.0f / T;
+        image = gameObject.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        sin = Mathf.Sin(2 * Mathf.PI * f * Time.time + Mathf.PI / 2);
+        // byteÇ…ïœä∑ÇµÅAîÕàÕÇ0Å`255Ç…Ç∑ÇÈ
+        byte alpha = (byte)((1 + sin) / 2 * 255);
+        newColor = image.color;
+        // 0.0Å`1.0Ç…ê≥ãKâª
+        newColor.a = alpha / 255f;
+        image.color = newColor;
+    }
+}

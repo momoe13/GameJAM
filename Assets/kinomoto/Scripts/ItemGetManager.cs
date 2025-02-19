9e6280c28f4ca5fe19d3f98aeea1874fbb5cece2
kinomoto-kinosan <c4100@oic.jp>
2025-02-20 03:28:53 +0900

    各種アイテムや景品を獲得した際に記録する
    スコアマネージャーに各テキストをアサインすることで、シーンを切り替えてもアイテムなどの獲得数を保持するように変更
    保持を確認するためNキーを押すことでリザルトシーンに画面遷移（仮機能）
    kinomotoSceneとresultSceneを移動させるためビルドセッティングでシーンを入れてある

--- Assets/kinomoto/Scripts/ItemGetManager.cs
+++ Assets/kinomoto/Scripts/ItemGetManager.cs
@@ -15,6 +15,7 @@ public class ItemGetManager : MonoBehaviour
         {
             Destroy(collision.gameObject);//オブジェクトを削除
             ScoreKeep.score += 10;//スコアを10点加算
+            ScoreKeep.sterScore += 1;
         }
         //基礎パワーアップアイテムを入れたときの処理
         if (collision.gameObject.tag == "BasePowerUp")
@@ -22,6 +23,7 @@ public class ItemGetManager : MonoBehaviour
             cranePowerErea.GetComponent<MagneticForceVariable>().AddBase();//クレーンの基礎パワーを増加
             Destroy(collision.gameObject);//オブジェクトを削除
             ScoreKeep.score += 100;//スコアを100点加算
+            ScoreKeep.basePowerUpScore += 1;
             Debug.Log("基礎値パワーアップ！");
         }
         //倍率パワーアップを入れたときの処理
@@ -30,6 +32,7 @@ public class ItemGetManager : MonoBehaviour
             cranePowerErea.GetComponent<MagneticForceVariable>().AddMagnification();//クレーンの倍率パワーを増加
             Destroy(collision.gameObject);//オブジェクトを削除
             ScoreKeep.score += 100;//スコアを100点加算
+            ScoreKeep.ratePowerUpScore += 1;
             Debug.Log("倍率パワーアップ！");
         }
         //横幅パワーアップを入れたときの処理
@@ -39,6 +42,7 @@ public class ItemGetManager : MonoBehaviour
             cranePowerErea.GetComponent<MagneticForceVariable>().ColliderSizeUp();
             Destroy(collision.gameObject);//オブジェクトを削除
             ScoreKeep.score += 100;//スコアを100点加算
+            ScoreKeep.widthPowerUpScore += 1;
             Debug.Log("横幅パワーアップ！");
         }
         //ターン数+1を入れたときの処理
@@ -47,6 +51,7 @@ public class ItemGetManager : MonoBehaviour
             turnManager.GetComponent<TurnManager>().TurnCountUp();//ターン+1
             Destroy(collision.gameObject);//オブジェクトを削除
             ScoreKeep.score += 100;//スコアを100点加算
+            ScoreKeep.turnRecoveryUpScore += 1;
             Debug.Log("ターン数+1！");
         }
         //目標アイテムを取得した際の処理
@@ -54,7 +59,7 @@ public class ItemGetManager : MonoBehaviour
         {
             Destroy(collision.gameObject);//オブジェクトを削除
             ScoreKeep.score += 1000;//スコアを1000点加算
-
+            ScoreKeep.prizeScore += 1;
             if (targetManager.GetComponent<TargetManager>().TargetNumber == int.Parse(collision.gameObject.name))
             {
                 Debug.Log("目標のアイテムをゲット！");
--- Assets/kinomoto/Scripts/ScoreKeep.cs
+++ Assets/kinomoto/Scripts/ScoreKeep.cs
@@ -1,5 +1,19 @@
 
+using UnityEditor;
+
 public static class ScoreKeep
 {
-    public static int score;
+    public static int score;//基本スコア
+
+    public static int sterScore;//スター取得数の変数
+
+    public static int basePowerUpScore;//基礎パワーアップアイテム取得数の変数
+
+    public static int ratePowerUpScore;//倍率パワーアップアイテム取得数の変数
+
+    public static int widthPowerUpScore;//横幅パワーアップアイテム取得数の変数
+
+    public static int turnRecoveryUpScore;//ターン回復アイテム取得数の変数
+
+    public static int prizeScore;//景品アイテム獲得数の変数
 }

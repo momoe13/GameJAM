f840137183762721f50b051707502a27a923acbe
momoe13 <131223093+momoe13@users.noreply.github.com>
2025-02-20 00:25:52 +0900

    プライズにタグ付け、アイテムゲットマネージャーのターゲットタグの判定を修正→ItemGetという名で保存しました。

--- Assets/kano/Script/TargetItem.cs
+++ Assets/kano/Script/TargetItem.cs
@@ -1,30 +1,41 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TargetItem : MonoBehaviour
 {
-    [SerializeField] List<GameObject> PrfbItem;//全アイテムの配列
+    [SerializeField] GameObject[] PrfbItems; //プライズアイテムの配列
 
-    [SerializeField]int[] ItemNumbers;　　//全アイテムの番号
+    [SerializeField] int[] ItemNumbers;　　//プライズアイテムの番号
 
-    [SerializeField]GameObject[] PushItem;//シーンに登場させるアイテム
+    [SerializeField] GameObject[] PowerItems;    //強化用アイテム
 
-    [SerializeField]int targetNum;　　　　//目標となるアイテム番号
+    [SerializeField] public GameObject[] PushItem;//シーンに登場させるアイテム
 
-    [SerializeField]int itemNum;        　//
+    [SerializeField]public int targetNum;　　　　//目標となるアイテム番号
+
+    [SerializeField]int itemNum;        　//使いまわし用変数
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.C))//Cを押して目標を指定（仮）
         {
             //目標アイテム設定
-            targetNum =  Random.Range(0, ItemNumbers.Length);
+            targetNum = Random.Range(0, ItemNumbers.Length);
 
-            //シーンに登場させるアイテム
-            PushItem[0] = PrfbItem[targetNum];
+            //シーンに登場させる目標アイテム
+            PushItem[0] = PrfbItems[targetNum];
 
-           //PrfbItem.Remove(targetNum);
+            //パワーアップアイテム
+            itemNum = Random.Range(0, PowerItems.Length);
+            PushItem[1] = PowerItems[itemNum];
+
+            //その他プライズ
+            for (int i = 0; i < PushItem.Length-2; i++) 
+            {
+                itemNum = Random.Range(0, ItemNumbers.Length);
+                PushItem[i+2] = PrfbItems[itemNum];
+            }
         }
+     //   Debug.Log("目標は" + PushItem[0].ToString());
 
     }
 }

08efafac2da35aab8294b0f64f3f48320a555b72
momoe13 <131223093+momoe13@users.noreply.github.com>
2025-02-20 13:25:52 +0900

    無駄なコメント削除

--- Assets/kinomoto/Scripts/ScoreManager.cs
+++ Assets/kinomoto/Scripts/ScoreManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -34,30 +32,5 @@ public class ScoreManager : MonoBehaviour
         ratePowerUpScoreText.text = "倍率パワーアップ獲得数:" + ScoreKeep.ratePowerUpScore.ToString();
         widthPowerUpScoreText.text = "横幅パワーアップ獲得数:" + ScoreKeep.widthPowerUpScore.ToString();
         turnRecoveryUpScoreText.text = "ターン回復アイテム獲得数:" + ScoreKeep.turnRecoveryUpScore.ToString();
-        //prizeScoreText.text = "景品の獲得数:" + ScoreKeep.prizeScore.ToString();
-    {  
-        //    Text scoreText = scoreObject.GetComponent<Text>();
-        //    Text sterScoreText = sterScoreObject.GetComponent<Text>();
-        //    Text basePowerUpScoreText = basePowerUpScoreObject.GetComponent<Text>();
-        //    Text ratePowerUpScoreText = ratePowerUpScoreObject.GetComponent<Text>();
-        //    Text widthPowerUpScoreText = widthPowerUpScoreObject.GetComponent<Text>();
-        //    Text turnRecoveryUpScoreText = turnRecoveryUpScoreObject.GetComponent<Text>();
-        //    Text prizeScoreText = prizeScoreObject.GetComponent<Text>();
-
-        //テキストの表示入替
-        scoreText.text = "スコア:" + ScoreKeep.score.ToString();
-        //    sterScoreText.text = "星の獲得数:" + ScoreKeep.sterScore.ToString();
-        //    basePowerUpScoreText.text = "基礎パワーアップ獲得数:" + ScoreKeep.basePowerUpScore.ToString();
-        //    ratePowerUpScoreText.text = "倍率パワーアップ獲得数:" + ScoreKeep.ratePowerUpScore.ToString();
-        //    widthPowerUpScoreText.text = "横幅パワーアップ獲得数:" + ScoreKeep.widthPowerUpScore.ToString();
-        //    turnRecoveryUpScoreText.text = "ターン回復アイテム獲得数:" + ScoreKeep.turnRecoveryUpScore.ToString();
-        //    prizeScoreText.text = "景品の獲得数:" + ScoreKeep.prizeScore.ToString();
-
-        //（仮）エスケープを押してリザルトに移動
-        //スコアの引継ぎ確認用
-        if (Input.GetKeyDown(KeyCode.N))
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("resultScene");
-        }
     }
 }

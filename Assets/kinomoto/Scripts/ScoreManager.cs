31d8a0cae4d0e39438cbad1f2f03bc986bfa86ee
kinomoto-kinosan <c4100@oic.jp>
2025-02-20 12:22:13 +0900

    メインのゲームシーンにテキストを追加。メインにリザルトシーンを追加
    メインゲームシーンにテキストを追加することでエラーが出ないように修正。星やアイテムなどのタグ付け忘れを修正し、正常に動作するように修正。リザルトシーンを追加し、スコアに応じてイメージの数が増える処理を入れている。

--- Assets/kinomoto/Scripts/ScoreManager.cs
+++ Assets/kinomoto/Scripts/ScoreManager.cs
@@ -11,13 +11,14 @@ public class ScoreManager : MonoBehaviour
     public GameObject ratePowerUpScoreObject = null;
     public GameObject widthPowerUpScoreObject = null;
     public GameObject turnRecoveryUpScoreObject = null;
-    public GameObject prizeScoreObject = null;
+    //public GameObject prizeScoreObject = null;
 
     private void Start()
     {
         //初期化
     }
     private void Update()
+<<<<<<< Updated upstream
     {  Text scoreText = scoreObject.GetComponent<Text>();
         //    Text sterScoreText = sterScoreObject.GetComponent<Text>();
         //    Text basePowerUpScoreText = basePowerUpScoreObject.GetComponent<Text>();
@@ -27,6 +28,36 @@ public class ScoreManager : MonoBehaviour
         //    Text prizeScoreText = prizeScoreObject.GetComponent<Text>();
 
         //テキストの表示入替
+=======
+<<<<<<< HEAD
+    {
+        Text scoreText = scoreObject.GetComponent<Text>();
+        Text sterScoreText = sterScoreObject.GetComponent<Text>();
+        Text basePowerUpScoreText = basePowerUpScoreObject.GetComponent<Text>();
+        Text ratePowerUpScoreText = ratePowerUpScoreObject.GetComponent<Text>();
+        Text widthPowerUpScoreText = widthPowerUpScoreObject.GetComponent<Text>();
+        Text turnRecoveryUpScoreText = turnRecoveryUpScoreObject.GetComponent<Text>();
+        //Text prizeScoreText = prizeScoreObject.GetComponent<Text>();
+
+        //テキストの表示入替
+        scoreText.text = ScoreKeep.score.ToString() + "てん";
+        sterScoreText.text = "ほし:" + ScoreKeep.sterScore.ToString();
+        basePowerUpScoreText.text = "基礎パワーアップ獲得数:" + ScoreKeep.basePowerUpScore.ToString();
+        ratePowerUpScoreText.text = "倍率パワーアップ獲得数:" + ScoreKeep.ratePowerUpScore.ToString();
+        widthPowerUpScoreText.text = "横幅パワーアップ獲得数:" + ScoreKeep.widthPowerUpScore.ToString();
+        turnRecoveryUpScoreText.text = "ターン回復アイテム獲得数:" + ScoreKeep.turnRecoveryUpScore.ToString();
+        //prizeScoreText.text = "景品の獲得数:" + ScoreKeep.prizeScore.ToString();
+=======
+    {  Text scoreText = scoreObject.GetComponent<Text>();
+        //    Text sterScoreText = sterScoreObject.GetComponent<Text>();
+        //    Text basePowerUpScoreText = basePowerUpScoreObject.GetComponent<Text>();
+        //    Text ratePowerUpScoreText = ratePowerUpScoreObject.GetComponent<Text>();
+        //    Text widthPowerUpScoreText = widthPowerUpScoreObject.GetComponent<Text>();
+        //    Text turnRecoveryUpScoreText = turnRecoveryUpScoreObject.GetComponent<Text>();
+        //    Text prizeScoreText = prizeScoreObject.GetComponent<Text>();
+
+        //テキストの表示入替
+>>>>>>> Stashed changes
         scoreText.text = "スコア:" + ScoreKeep.score.ToString();
         //    sterScoreText.text = "星の獲得数:" + ScoreKeep.sterScore.ToString();
         //    basePowerUpScoreText.text = "基礎パワーアップ獲得数:" + ScoreKeep.basePowerUpScore.ToString();
@@ -34,6 +65,10 @@ public class ScoreManager : MonoBehaviour
         //    widthPowerUpScoreText.text = "横幅パワーアップ獲得数:" + ScoreKeep.widthPowerUpScore.ToString();
         //    turnRecoveryUpScoreText.text = "ターン回復アイテム獲得数:" + ScoreKeep.turnRecoveryUpScore.ToString();
         //    prizeScoreText.text = "景品の獲得数:" + ScoreKeep.prizeScore.ToString();
+<<<<<<< Updated upstream
+=======
+>>>>>>> a0cc5b4139dc7bfd90e65c1f1752929886b717f2
+>>>>>>> Stashed changes
 
 
 

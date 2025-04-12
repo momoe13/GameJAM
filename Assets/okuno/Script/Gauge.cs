37e7b844fd6102741b28c9e0286c31714a33a5aa
Lykan55 <c4073@oic.jp>
2025-04-13 00:25:03 +0900

    上限突破のアニメーション仮完成
    その他
    不要な変数を削除
    冗長なコードを削除

--- Assets/okuno/Script/Gauge.cs
+++ Assets/okuno/Script/Gauge.cs
@@ -11,13 +11,13 @@ public class Gauge : MonoBehaviour
         TWO,
         THREE,
         FOUR,
+        BURST,
     }
     private State state;
     [SerializeField] private GameObject powerErea;
     [Header("ゲージが上がる値")]
     [SerializeField] private float[] craneLevelUpValue;
     [SerializeField] private GameObject[] gaugeAry;
-    private int idx = 0;
 
     private void Start()
     {
@@ -39,27 +39,36 @@ public class Gauge : MonoBehaviour
             switch (state)
             {
                 case State.ZERO:
+                    //craneLevelUpValueの配列にあるState番目の値より、クレーンの吸引力が大きい場合ゲージが一つ上がる
+                    //forceMagnitudeの値はマイナスが大きければ大きいほど吸引力が強い
                     if(powerErea.GetComponent<PointEffector2D>().forceMagnitude > craneLevelUpValue[(int)State.ZERO]) { break; }
-                    gaugeAry[idx].gameObject.SetActive(true);
-                    idx++;
+                    //一段階目
+                    gaugeAry[(int)State.ZERO].gameObject.SetActive(true);
                     state = State.ONE;
                     break;
                 case State.ONE:
                     if (powerErea.GetComponent<PointEffector2D>().forceMagnitude > craneLevelUpValue[(int)State.ONE]) { break; }
-                    gaugeAry[idx].gameObject.SetActive(true);
-                    idx++;
+                    //二段階目
+                    gaugeAry[(int)State.ONE].gameObject.SetActive(true);
                     state = State.TWO;
                     break;
                 case State.TWO:
                     if (powerErea.GetComponent<PointEffector2D>().forceMagnitude > craneLevelUpValue[(int)State.TWO]) { break; }
-                    gaugeAry[idx].gameObject.SetActive(true);
-                    idx++;
+                    //三段階目
+                    gaugeAry[(int)State.TWO].gameObject.SetActive(true);
                     state = State.THREE;
                     break;
                 case State.THREE:
                     if (powerErea.GetComponent<PointEffector2D>().forceMagnitude > craneLevelUpValue[(int)State.THREE]) { break; }
-                    gaugeAry[idx].gameObject.SetActive(true);
+                    //四段階目
+                    gaugeAry[(int)State.THREE].gameObject.SetActive(true);
                     state = State.FOUR;
+                    break;
+                case State.FOUR:
+                    if (powerErea.GetComponent<PointEffector2D>().forceMagnitude > craneLevelUpValue[(int)State.FOUR]) { break; }
+                    //上限突破
+                    gaugeAry[(int) State.FOUR].gameObject.SetActive(true);
+                    state = State.BURST;
                     break;
             }
             yield return null;
@@ -68,11 +77,10 @@ public class Gauge : MonoBehaviour
 
     public void GaugeReset()
     {
-        for (idx = 3; idx >= 0; idx--)
+        foreach (var gauge in gaugeAry)
         {
-            gaugeAry[idx].SetActive(false);
+            gauge.SetActive(false);
         }
-        idx = 0;
         state = State.ZERO;
     }
 }
